1fd7f692d394611fff8d10d51491692db2f43ffe
pablo-maff <pablo.maff.076@gmail.com>
2021-12-08 23:42:28 +0100

    Linked List hecho

--- 04-EstructuraDeDatos-II/homework/homework.js
+++ 04-EstructuraDeDatos-II/homework/homework.js
@@ -15,77 +15,76 @@ function LinkedList() {
   this.head = null; // Puntero
 }
 
+
 function Node(value){
   this.value = value; // Datos
   this.next = null; // Puntero
 }
 
+
 LinkedList.prototype.add = function(data) {
-  let node = new Node(data), // Initialize node
-  current = this.head;  // Pointer
+  let node = new Node(data), // Instanciate node
+  pointer = this.head;       // Initialize pointer set to head
   // If list is empty
-  if (!current) {
-    this.head = node; // The head pointer now is pointing to the new node
-    this._length++;  // Length +1
+  if (!pointer) {
+    this.head = node; // Set head to the new node
+    this._length++;
     return node;
   }
   // If not empty, traverse the whole list to find the last node
-  while (current.next != null) {
-    current = current.next; // Set pointer on the last node of the list
+  while (pointer.next) {
+    pointer = pointer.next; // Set pointer to the last node of the list
   }
-  current.next = node; // Add new node at the end of the list
-  this._length++; // Length +1
+  pointer.next = node; // Add new node at the end of the list
+  this._length++;
   return node;
 };
+
 
 LinkedList.prototype.remove = function() {
   let pointer = this.head; // Set pointer to head
   // If list is empty return null
   if (!pointer) return null;
-  // If list has 1 element, return element vale and remove it.
-  if (pointer.next == null) {  // if next is null means that there is no next, therefore is a 1 element list
-    this.head = null  // Set node to null
-    return pointer.value; // return removed node value
+  // If list has 1 element, return element value and remove it.
+  if (!pointer.next) {  // if next is null means that there is no next, therefore is a 1 element list
+    this.head = null    // Set node to null
+    --this._length;
+    return pointer.value; // $eturn removed node value
   }
   // If two positions ahead of the pointer is null, we found the end of the list!
-  while (pointer.next.next != null) { 
+  while (pointer.next.next) { 
 		pointer = pointer.next; // Set pointer to the second last node
 	}
   let aux = pointer.next.value // Save last node value
-	pointer.next = null; // Make last node null (remove)
-	this._length--; // Length -1
-  return aux; // Return node value
+	pointer.next = null;         // Make last node null (remove)
+	--this._length;
+  return aux; // Return removed node value
 }
+
 
 LinkedList.prototype.search = function(val) {
-	let pointer = this.head; // Set pointer to head of the list
+  let pointer = this.head; // Set pointer to head of the list
   // If list is empty
 	if (!pointer) return null;
-	let check = false; // Flag
-  // If val is found
-	if (pointer.value == val) check = true; // Set flag as true
-  // If val not found traverse the list until the end
-	while (!check && pointer.next != null) {
+  // If val is found return it.
+	else if (pointer.value == val) return pointer.value;
+  // If val is a function and its invocation is true (Only if val is the head of the list, otherwise will search for it in the loop)
+  else if (typeof val == 'function' && val(pointer.value)) {
+    return pointer.value // Return value
+  }
+  // If val not found traverse the list to search for val until the end
+  while (pointer.next != null) {
 		pointer = pointer.next; // Set pointer one position ahead with each iteration
-		if (pointer.value == val) check = true; // If val is found, set flag as true
-	}
-  // If val is in the list return val
-	if (check) {
-		return pointer.value;
-  // If not found return undefined
-	} else {
-		return null;	
-	} 
+		// If val is found, return it
+    if (pointer.value == val) return pointer.value; 
+    // If val is a function and its invocation is true
+	  else if (typeof val == 'function' && val(pointer.value)) {    
+      return pointer.value // Return value 
+    }
+  }
+  // If not found return null
+	return null;	
 }
-
-
-let linkedList = new LinkedList();
-console.log(linkedList.add('one'));
-console.log(linkedList.add('two'));
-console.log(linkedList.search(function(nodeValue) {
-  return nodeValue === 'two';
-}))
-console.log(linkedList)
 
 
 // Hash Table( ver información en: https://es.wikipedia.org/wiki/Tabla_hash)
